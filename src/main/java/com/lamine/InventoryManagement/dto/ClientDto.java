4fc413f0911f39497aba12dcdfb407e2592005de
laminejuve <laminejuvee@gmail.com>
2022-04-25 10:37:14 +0100

    creating the DTOs

--- src/main/java/com/lamine/InventoryManagement/dto/ClientDto.java
+++ src/main/java/com/lamine/InventoryManagement/dto/ClientDto.java
@@ -1,6 +1,7 @@
 package com.lamine.InventoryManagement.dto;
 
 import com.lamine.InventoryManagement.model.Adresse;
+import com.lamine.InventoryManagement.model.Client;
 import lombok.Builder;
 import lombok.Data;
 import java.util.List;
@@ -17,4 +18,37 @@ public class ClientDto {
     private String mail ;
     private String numTel ;
     private List<CommandeClientDto> commandeClients ;
+
+    public ClientDto fromEntity (Client client){
+
+        if (client == null){
+            // do an exception
+            return null;
+        }
+       return  ClientDto.builder()
+                .id(client.getId())
+                .name(client.getName())
+                .prenom(client.getPrenom())
+                .adresse(client.getAdresse())
+                .mail(client.getMail())
+                .numTel(client.getNumTel())
+                .photo(client.getPhoto())
+                .build();
+    }
+    public Client toEntity(ClientDto clientDto){
+
+        if (clientDto== null){
+            // to do an exception
+            return null;
+        }
+        Client client = new Client();
+        client.setId(clientDto.getId()) ;
+        client.setName(clientDto.getName());
+        client.setPrenom(clientDto.getPrenom());
+        client.setAdresse(clientDto.getAdresse());
+        client.setMail(clientDto.getMail());
+        client.setPhoto(clientDto.getPhoto());
+        client.setNumTel(clientDto.getNumTel());
+        return client;
+    }
 }
