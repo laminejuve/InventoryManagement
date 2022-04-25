9f6cddfe4e1715d74e6677eceea902852e9e64ef
laminejuve <laminejuvee@gmail.com>
2022-04-25 14:55:22 +0100

    Adding to the user DTO

--- src/main/java/com/lamine/InventoryManagement/dto/UserDto.java
+++ src/main/java/com/lamine/InventoryManagement/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.lamine.InventoryManagement.dto;
 
+import com.lamine.InventoryManagement.model.User;
 import lombok.Builder;
 import lombok.Data;
 import java.util.Date;
@@ -20,4 +21,39 @@ public class UserDto {
     private String numTel ;
     private List<RolesDto> roles ;
     private EntrepriseDto entreprise;
+
+    public UserDto fromEntity (User user){
+        if (user == null ){
+            //TODO an exception
+            return null ;
+        }
+         return UserDto.builder()
+                .id(user.getId())
+                .nom(user.getNom())
+                .prenom(user.getPrenom())
+                .dateNaissance(user.getDateNaissance())
+                .motDePasse(user.getMotDePasse())
+                .photo(user.getPhoto())
+                .mail((user.getMail()))
+                .numTel(user.getNumTel())
+                .build();
+    }
+
+    public User toEntity (UserDto userDto){
+        if (userDto == null){
+            // TODO an exception
+            return null ;
+        }
+        User user = new User();
+        user.setId(userDto.getId());
+        user.setNom(userDto.getNom());
+        user.setPrenom(userDto.getPrenom());
+        user.setDateNaissance(userDto.getDateNaissance());
+        user.setMotDePasse(userDto.getMotDePasse());
+        user.setPhoto(userDto.getPhoto());
+        user.setMail(userDto.getMail());
+        user.setNumTel(userDto.getNumTel());
+
+        return user;
+    }
 }
