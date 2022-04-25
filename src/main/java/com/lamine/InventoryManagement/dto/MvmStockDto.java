50a2176cd9961ef00e3e8bff7966101c02f87e7f
laminejuve <laminejuvee@gmail.com>
2022-04-25 14:30:49 +0100

    Adding the DTOs

--- src/main/java/com/lamine/InventoryManagement/dto/MvmStockDto.java
+++ src/main/java/com/lamine/InventoryManagement/dto/MvmStockDto.java
@@ -1,5 +1,6 @@
 package com.lamine.InventoryManagement.dto;
 
+import com.lamine.InventoryManagement.model.MvmStock;
 import com.lamine.InventoryManagement.model.TypeMvtStk;
 import lombok.Builder;
 import lombok.Data;
@@ -16,5 +17,28 @@ public class MvmStockDto {
     private BigDecimal quantity;
     private ArticleDto article;
     private TypeMvtStk typeMvt ;
+
+    public MvmStockDto fromEntity (MvmStock mvmStock){
+        if (mvmStock == null){
+            // TODO an exception
+            return null ;
+        }
+        return MvmStockDto.builder()
+                .id(mvmStock.getId())
+                .dateMvt(mvmStock.getDateMvt())
+                .quantity(mvmStock.getQuantity())
+                .build();
+    }
+    public MvmStock toEntity (MvmStockDto mvmStockDto){
+        if (mvmStockDto ==  null ){
+            //TODO an exception
+            return null ;
+        }
+        MvmStock mvmStock = new MvmStock();
+        mvmStock.setId(mvmStockDto.getId());
+        mvmStock.setDateMvt(mvmStockDto.getDateMvt());
+        mvmStock.setQuantity(mvmStockDto.getQuantity());
+        return mvmStock ;
+    }
 }
 
--- src/main/java/com/lamine/InventoryManagement/dto/RolesDto.java
+++ src/main/java/com/lamine/InventoryManagement/dto/RolesDto.java
@@ -1,5 +1,6 @@
 package com.lamine.InventoryManagement.dto;
 
+import com.lamine.InventoryManagement.model.Roles;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,4 +12,26 @@ public class RolesDto {
     private Integer id ;
     private String roleName;
     private UserDto user ;
+
+    public RolesDto fromEntity (Roles roles){
+        if (roles == null){
+            //TODO an exception
+            return null ;
+        }
+        return RolesDto.builder()
+                .id(roles.getId())
+                .roleName(roles.getRoleName())
+                .build();
+    }
+
+    public Roles toEntity (RolesDto rolesDto){
+        if (rolesDto == null){
+            //TODO an exception
+            return null;
+        }
+        Roles roles = new Roles();
+        roles.setId(rolesDto.getId());
+        roles.setRoleName(rolesDto.getRoleName());
+        return roles;
+    }
 }
