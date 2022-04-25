aa5d916b9e33ea254b247167bece8ff9db37dcb3
laminejuve <laminejuvee@gmail.com>
2022-04-25 12:30:58 +0100

    Adding the DTOs

--- src/main/java/com/lamine/InventoryManagement/dto/EntrepriseDto.java
+++ src/main/java/com/lamine/InventoryManagement/dto/EntrepriseDto.java
@@ -1,6 +1,7 @@
 package com.lamine.InventoryManagement.dto;
 
 import com.lamine.InventoryManagement.model.Adresse;
+import com.lamine.InventoryManagement.model.Entreprise;
 import com.lamine.InventoryManagement.model.User;
 import lombok.Builder;
 import lombok.Data;
@@ -20,4 +21,40 @@ public class EntrepriseDto {
     private String numTel ;
     private String siteWeb ;
     private List<UserDto> users ;
+
+    public EntrepriseDto fromEntity (Entreprise entreprise){
+        if(entreprise == null ){
+            // TODO an exception
+            return null ;
+        }
+        return EntrepriseDto.builder()
+                .id(entreprise.getId())
+                .nom(entreprise.getNom())
+                .description(entreprise.getDescription())
+                .photo(entreprise.getPhoto())
+                .codeFiscal(entreprise.getCodeFiscal())
+                .email(entreprise.getEmail())
+                .numTel(entreprise.getNumTel())
+                .siteWeb(entreprise.getSiteWeb())
+                .build();
+    }
+
+    public Entreprise toEntity (EntrepriseDto entrepriseDto){
+
+        if (entrepriseDto == null ){
+            // TODO an exception
+            return null;
+        }
+        Entreprise entreprise = new Entreprise();
+        entreprise.setId((entrepriseDto.getId()));
+        entreprise.setNom(entreprise.getNom());
+        entreprise.setDescription(entrepriseDto.getDescription());
+        entreprise.setCodeFiscal(entrepriseDto.getCodeFiscal());
+        entreprise.setEmail(entrepriseDto.getEmail());
+        entreprise.setPhoto(entrepriseDto.getPhoto());
+        entreprise.setNumTel(entrepriseDto.getNumTel());
+        entreprise.setSiteWeb(entrepriseDto.getSiteWeb());
+
+        return entreprise;
+    }
 }
--- src/main/java/com/lamine/InventoryManagement/dto/FornisseurDto.java
+++ src/main/java/com/lamine/InventoryManagement/dto/FornisseurDto.java
@@ -1,7 +1,9 @@
 package com.lamine.InventoryManagement.dto;
 
 import com.lamine.InventoryManagement.model.Adresse;
+import com.lamine.InventoryManagement.model.Client;
 import com.lamine.InventoryManagement.model.CommandeFornisseur;
+import com.lamine.InventoryManagement.model.Fornisseur;
 import lombok.Builder;
 import lombok.Data;
 
@@ -21,4 +23,35 @@ public class FornisseurDto {
     private String mail ;
     private String numTel ;
     private List<CommandeFornisseurDto> commandeFornisseurs;
+
+    public FornisseurDto fromEntity (Fornisseur fornisseur){
+
+        if (fornisseur == null){
+            // do an exception
+            return null;
+        }
+        return  FornisseurDto.builder()
+                .id(fornisseur.getId())
+                .nom(fornisseur.getNom())
+                .prenom(fornisseur.getPrenom())
+                .mail(fornisseur.getMail())
+                .numTel(fornisseur.getNumTel())
+                .photo(fornisseur.getPhoto())
+                .build();
+    }
+    public Fornisseur toEntity(FornisseurDto fornisseurDto){
+
+        if (fornisseurDto == null){
+            // to do an exception
+            return null;
+        }
+        Fornisseur fornisseur = new Fornisseur();
+        fornisseur.setId(fornisseurDto.getId()) ;
+        fornisseur.setNom(fornisseurDto.getNom());
+        fornisseur.setPrenom(fornisseurDto.getPrenom());
+        fornisseur.setMail(fornisseurDto.getMail());
+        fornisseur.setPhoto(fornisseurDto.getPhoto());
+        fornisseur.setNumTel(fornisseurDto.getNumTel());
+        return fornisseur;
+    }
 }
