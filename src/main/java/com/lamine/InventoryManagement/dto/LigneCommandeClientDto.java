3a1888bbabdf8ff3b7cdadf5be3764d1a5a0f293
laminejuve <laminejuvee@gmail.com>
2022-04-25 13:06:49 +0100

    Adding the DTOs

--- src/main/java/com/lamine/InventoryManagement/dto/LigneCommandeClientDto.java
+++ src/main/java/com/lamine/InventoryManagement/dto/LigneCommandeClientDto.java
@@ -1,5 +1,6 @@
 package com.lamine.InventoryManagement.dto;
 
+import com.lamine.InventoryManagement.model.LigneCommandeClient;
 import lombok.Builder;
 import lombok.Data;
 
@@ -14,4 +15,27 @@ public class LigneCommandeClientDto {
     private CommandeClientDto commandeClient;
     private BigDecimal quantity;
     private BigDecimal prixUnitaire;
+
+    public LigneCommandeClientDto fromEntity (LigneCommandeClient ligneCommandeClient){
+        if (ligneCommandeClient == null ){
+            // TODO an exception
+            return null ;
+        }
+        return LigneCommandeClientDto.builder()
+                .id(ligneCommandeClient.getId())
+                .quantity(ligneCommandeClient.getQuantity())
+                .prixUnitaire(ligneCommandeClient.getPrixUnitaire())
+                .build();
+    }
+    public LigneCommandeClient toEntity (LigneCommandeClientDto ligneCommandeClientDto){
+        if(ligneCommandeClientDto == null ){
+            //TODO an exception
+            return null ;
+        }
+        LigneCommandeClient ligneCommandeClient = new LigneCommandeClient();
+        ligneCommandeClient.setId(ligneCommandeClientDto.getId());
+        ligneCommandeClient.setQuantity(ligneCommandeClientDto.getQuantity());
+        ligneCommandeClient.setPrixUnitaire(ligneCommandeClientDto.getPrixUnitaire());
+        return ligneCommandeClient ;
+    }
 }
--- src/main/java/com/lamine/InventoryManagement/dto/LigneCommandeFornisseurDto.java
+++ src/main/java/com/lamine/InventoryManagement/dto/LigneCommandeFornisseurDto.java
@@ -2,6 +2,7 @@ package com.lamine.InventoryManagement.dto;
 
 import com.lamine.InventoryManagement.model.Article;
 import com.lamine.InventoryManagement.model.CommandeFornisseur;
+import com.lamine.InventoryManagement.model.LigneCommandeFornisseur;
 import lombok.Builder;
 import lombok.Data;
 
@@ -17,6 +18,29 @@ public class LigneCommandeFornisseurDto {
     private ArticleDto article;
     private CommandeFornisseurDto commandeFornisseur;
     private BigDecimal quantity;
-    private BigDecimal prixUitaire ;
+    private BigDecimal prixUnitaire ;
+
+    public LigneCommandeFornisseurDto fromEntity (LigneCommandeFornisseur ligneCommandeFornisseur){
+        if (ligneCommandeFornisseur == null ){
+            // TODO an exception
+            return null ;
+        }
+        return LigneCommandeFornisseurDto.builder()
+                .id(ligneCommandeFornisseur.getId())
+                .quantity(ligneCommandeFornisseur.getQuantity())
+                .prixUnitaire(ligneCommandeFornisseur.getPrixUitaire())
+                .build();
+    }
+    public LigneCommandeFornisseur toEntity (LigneCommandeFornisseurDto ligneCommandeFornisseurDto){
+        if(ligneCommandeFornisseurDto == null ){
+            //TODO an exception
+            return null ;
+        }
+        LigneCommandeFornisseur ligneCommandeFornisseur = new LigneCommandeFornisseur();
+        ligneCommandeFornisseur.setId(ligneCommandeFornisseurDto.getId());
+        ligneCommandeFornisseur.setQuantity(ligneCommandeFornisseurDto.getQuantity());
+        ligneCommandeFornisseur.setPrixUitaire(ligneCommandeFornisseurDto.getPrixUnitaire());
+        return ligneCommandeFornisseur ;
+    }
 
 }
