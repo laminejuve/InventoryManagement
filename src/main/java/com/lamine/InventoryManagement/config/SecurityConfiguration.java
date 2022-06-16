26c87eac9a0ff00bdaedb435675a3465e71de4c7
laminejuve <laminejuvee@gmail.com>
2022-06-16 14:38:14 +0100

    Adding Jwt Request Filter

--- src/main/java/com/lamine/InventoryManagement/config/SecurityConfiguration.java
+++ src/main/java/com/lamine/InventoryManagement/config/SecurityConfiguration.java
@@ -1,23 +1,29 @@
 package com.lamine.InventoryManagement.config;
 
 import com.lamine.InventoryManagement.service.auth.MyUserDetailsService;
+import com.lamine.InventoryManagement.utils.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     MyUserDetailsService myUserDetailsService;
+    @Autowired
+    JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -28,12 +34,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests()
                 .antMatchers("/**/authenticate/token"
-                                        ,"/**/swagger-ui/**"
-                ).permitAll()
+                                        ,"/**/swagger-ui/**")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin();
+
+        http.addFilterBefore(jwtRequestFilter , UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
